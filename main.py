95c8aacab0ac7d0204b91a7b23128e128d68b33f
200054082studentkangan <200054082@student.kangan.edu.au>
2024-11-21 13:22:05 +1100

    fixed selected d20

--- main.py
+++ main.py
@@ -6,8 +6,26 @@ import dice
 
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
-dice_type = 2
+dice_type = 20
 
+dice_type = document.getElementById("diesides").value 
+print (dice_type)
+if dice_type == "D2":
+    dice_type = 2 
+elif dice_type == "D4":
+    dice_type = 4 
+elif dice_type == "D6":
+    dice_type= 6
+elif dice_type == "D8":
+    dice_type = 8
+elif dice_type== "D10":
+    dice_type = 10
+elif dice_type == "D12":
+    dice_type = 12
+elif dice_type == "D20":
+    dice_type = 20 
+elif dice_type == "D100":
+    dice_type = 100
 
 
 def select_face_option(event):
